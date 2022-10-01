08a5fedb8ee4a55341a891292c8ba8283cd380a9
Abdiel Corro <acorrop@outlook.com>
2022-10-01 00:13:00 -0500

    exercise 1 completed

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "Toyota";
+            car.Model = "Solara";
+            car.Year = 2005;
+
+            Console.WriteLine($"My car is a {car.Year} {car.Make} {car.Model}");
+
+        }
+    }
+}
